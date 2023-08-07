665a938e96a83866bf7c459ca065dbc5ae7b84a3
Daniel Errington <derrington@gmail.com>
2023-08-07 09:23:24 +0100

    Add retries for Chromium connection

--- src/chrome-debug-connector.cpp
+++ src/chrome-debug-connector.cpp
@@ -2,9 +2,12 @@
 #include "curl-wrapper.hpp"
 #include "exceptions.hpp"
 
-#include <string>
 #include <fmt/core.h>
+
+#include <chrono>
 #include <iostream>
+#include <string>
+#include <thread>
 
 using namespace std;
 using namespace web_video_capture;
@@ -20,16 +23,30 @@ chrome_debug_connector::chrome_debug_connector(int port)
 
 string get_debug_info_json(int port)
 {
-	auto const debug_info_address = format("http://localhost:{}/json", port);
+	auto const debug_info_address = fmt::format("http://localhost:{}/json", port);
 
 	auto const curl = ::curl();
 
-	string response_buffer;
-	curl.set_option(CURLOPT_URL, debug_info_address.c_str());
-	curl.set_option(CURLOPT_WRITEFUNCTION, &write_curl_buffer);
-	curl.set_option(CURLOPT_WRITEDATA, &response_buffer);
+	auto try_count = 0;
+	auto has_connection = false;
 
-	if (curl.perform() != CURLE_OK)
+	string response_buffer;
+
+	do
+	{
+		if(try_count > 0)
+		{
+			this_thread::sleep_for(chrono::seconds(1));
+		}
+
+		curl.set_option(CURLOPT_URL, debug_info_address.c_str());
+		curl.set_option(CURLOPT_WRITEFUNCTION, &write_curl_buffer);
+		curl.set_option(CURLOPT_WRITEDATA, &response_buffer);
+
+		has_connection = curl.perform() == CURLE_OK;
+	} while (!has_connection && ++try_count < 10);
+
+	if (!has_connection)
 	{
 		throw chrome_connection_failed_exception();
 	}
