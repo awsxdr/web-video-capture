f1702fb9b59a95eb2272ec2286fc90c39d5830fa
Daniel Errington <derrington@gmail.com>
2023-08-13 22:46:02 +0100

    Improve Linux build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 build_type="${1:-Debug}"
 
-rm -rf ./build
-cmake -B ./build -G "Ninja" -DCMAKE_BUILD_TYPE=$build_type
-cmake --build ./build --config $build_type
+cmake -B ./build/$build_type -G "Ninja" -DCMAKE_BUILD_TYPE=$build_type
+cmake --build ./build/$build_type --config $build_type
